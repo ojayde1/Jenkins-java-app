e42ce1783d32fa135e2d08e7bcf08a20e554437e
Olajide <ojayde35@gmail.com>
2025-10-12 21:39:40 -0500

    fix

--- src/test/java/com/devjide/demo/JavaAppTests.java
+++ src/test/java/com/devjide/demo/JavaAppTests.java
@@ -1,17 +1,3 @@
-package com.javatechie.k8s;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SpringbootK8sDemoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
-
 package com.devjide.demo; //to match the pom.xml group ID
 
 import org.junit.jupiter.api.Test;
