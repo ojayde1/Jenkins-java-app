dc6a15127c6e24d9ff8181a8ca1c459a9f63d146
Olajide <ojayde35@gmail.com>
2025-10-21 20:15:40 -0500

    General changes for new push

--- src/main/java/com/devjide/demo/JenkinJavaApp.java
+++ src/main/java/com/devjide/demo/JenkinJavaApp.java
@@ -1,44 +1,14 @@
 
-// package com.devjide.demo; 
-
-// import org.springframework.boot.SpringApplication;
-// import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RestController;
-
-// @SpringBootApplication
-// @RestController
-// public class JenkinJavaApp { // RENAMED: More descriptive class name
-
-//     /**
-//      * Defines a simple GET endpoint to confirm the application is running.
-//      * Accessible at http://<host>:8080/message
-//      * @return A congratulatory message string.
-//      */
-//     @GetMapping("/message")
-//     public String displayMessage(){
-//         // Using the artifactId in the message to confirm which app is running
-//         return "Congratulations! The 'Jenkin-Java-App' is successfully deployed via CI/CD!";
-//     }
-
-//     public static void main(String[] args) {
-//         SpringApplication.run(JenkinJavaApp.class, args);
-//     }
-
-// }
-
-package com.devjide.demo;
+package com.devjide.demo; 
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer; // <-- NEW IMPORT
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-// CRITICAL: Extends the initializer to allow WAR deployment to external Tomcat
-public class JenkinJavaApp extends SpringBootServletInitializer { 
+public class JenkinJavaApp { // RENAMED: More descriptive class name
 
     /**
      * Defines a simple GET endpoint to confirm the application is running.
@@ -56,4 +26,34 @@ public class JenkinJavaApp extends SpringBootServletInitializer {
     }
 
 }
+
+// package com.devjide.demo;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.web.servlet.support.SpringBootServletInitializer; // <-- NEW IMPORT
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
+
+// @SpringBootApplication
+// @RestController
+// // CRITICAL: Extends the initializer to allow WAR deployment to external Tomcat
+// public class JenkinJavaApp extends SpringBootServletInitializer { 
+
+//     /**
+//      * Defines a simple GET endpoint to confirm the application is running.
+//      * Accessible at http://<host>:8080/message
+//      * @return A congratulatory message string.
+//      */
+//     @GetMapping("/message")
+//     public String displayMessage(){
+//         // Using the artifactId in the message to confirm which app is running
+//         return "Congratulations! The 'Jenkin-Java-App' is successfully deployed via CI/CD!";
+//     }
+
+//     public static void main(String[] args) {
+//         SpringApplication.run(JenkinJavaApp.class, args);
+//     }
+
+// }
 
